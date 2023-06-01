8d84f7822abb762e040f6328760d1099a01a5c07
AguJP <agujpa@gmail.com>
2023-06-01 06:36:58 -0700

    Is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -16,7 +16,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	}
 	else
 	{
-		if (tree->left && tree-right)
+		if (tree->left && tree->right)
 		{
 			left = binary_tree_is_full(tree->left);
 			right = binary_tree_is_full(tree->right);
